1ca1a19a3cadfb27b6fb24ffeb7b7052292a26b4
Marcel Mamula <marcel.mamula@suse.com>
2026-02-26 12:19:34 +0100

    revert fstring back to format for compatibility

--- plugins/modules/sap_system_facts.py
+++ plugins/modules/sap_system_facts.py
@@ -270,11 +270,7 @@ def run_module():
     # Fail if execution user does not have permission for sapcontrol
     sapcontrol_path = module.get_bin_path('/usr/sap/hostctrl/exe/sapcontrol', required=True)
     if not os.access(sapcontrol_path, os.X_OK):
-<<<<<<< HEAD
         module.fail_json(msg="Permission denied: Ansible user cannot execute {0}".format(sapcontrol_path))
-=======
-        module.fail_json(msg=f"Permission denied: Ansible user cannot execute {sapcontrol_path}")
->>>>>>> 2e99adf (add SID and permission check to facts module)
 
     hana_sid = get_all_hana_sid()
     if hana_sid:
